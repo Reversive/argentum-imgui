f846a37b10257b52c1157ab307f69003815cf603
Reversive <matiaspavan@gmail.com>
2022-03-22 21:43:38 -0300

    feat: added minhook and imgui dependencies

---
+++ argentum-imgui/argentum/argentum.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <d3d9.h>
+#include <minhook/minhook.h>
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_dx9.h>
+#include <imgui/imgui_impl_win32.h>
+
+
+#if defined( _WIN32 ) || defined( _WIN64 )
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
